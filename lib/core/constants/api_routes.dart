4a166273675722dec4c4376b1c6eae79dfe6e8ea
marvel <ikpmarvel@gmail.com>
2025-08-09 19:02:27 +0100

    changed remote url

--- lib/core/constants/api_routes.dart
+++ lib/core/constants/api_routes.dart
@@ -1,7 +1,7 @@
 class ApiRoute {
   /// Base Url
   static const String baseUrl =
-      'https://vinkol-server.onrender.com/api/v1'; // "https://vinkol-server-staging.vercel.app/api/v1";
+      "https://vinkol-server-staging.vercel.app/api/v1"; //  'https://vinkol-server.onrender.com/api/v1';
 
   // Auth Url
   static const String login = "$baseUrl/users/login";
